297bccc2b8dc97e77ed103b59c5f04c65f5a519a
Sooyeon Cho <sy.cho2321@gmail.com>
2022-01-12 09:00:40 +0100

    initial commit

---
+++ src/vocab_list_generator.py
@@ -0,0 +1,54 @@
+"""
+This script generate vocabulary list with its frequency based on the set of tokenized texts
+"""
+from src.data_reader import read_texts_into_lists
+import pandas as pd
+from src.korean_l2_tokenizer import tokenize, STOPWORDS_POS
+from collections import Counter
+
+
+def flatten_list(txts):
+    """
+    input: list of texts
+    output: flattend string (list concatenated)
+    :param txts:
+    :return:
+    """
+    flatten = str()
+    for txt in txts:
+        flatten = flatten + txt
+    return flatten
+
+
+if __name__ == '__main__':
+    PATH = '../data'
+    vocab_df = pd.DataFrame(columns=['vocab', 'pos', 'frequency'])
+
+    # read data
+    txt_id, txt_list = read_texts_into_lists(PATH)
+
+    # flatten list of tokenized texts
+    raw_flatten = flatten_list(txt_list)
+
+    # tokenize
+    pos_tuple, _ = tokenize(raw_flatten, content_only=False)
+
+    # clean pos_tuple (exclude stopwords from pos_tuple)
+    pos_tuple_clean = [pos for pos in pos_tuple if pos[1] not in STOPWORDS_POS]
+
+    # calculate frequency
+    pos_with_frequency = Counter(pos_tuple_clean)
+    pos_with_frequency = pos_with_frequency.most_common()  # sort by frequency
+
+    # separate token and pos
+    # tokens = [item[0] for item in pos_tuple_clean]
+    # pos = [item[1] for item in pos_tuple_clean]
+
+    # put in df
+    vocab_df['vocab'] = [item[0][0] for item in pos_with_frequency]
+    vocab_df['pos'] = [item[0][1] for item in pos_with_frequency]
+    vocab_df['frequency'] = [item[1] for item in pos_with_frequency]
+
+
+    # to excel
+    vocab_df.to_excel("vocab_list.xlsx", encoding='utf-8')
