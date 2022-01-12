cbe22370824f4d907ea5c1ddd0457a653857927c
Sooyeon Cho <sy.cho2321@gmail.com>
2022-01-12 23:13:47 +0100

    minor change: docstring

--- src/ld_analyser.py
+++ src/ld_analyser.py
@@ -6,6 +6,7 @@ from src.ld_calculator import calculate_all_ld
 # todo 2: param context_window usw. add (now default setting)
 
 """
+This script makes excel file with all lexical diversity scores for each text
 Before running this script!
 1. change PATH variable to the directory where texts exist
 2. change CONTENT_ONLY variable (boolean) - if true, only take content words, if false, analyse all words
@@ -14,6 +15,7 @@ Before running this script!
 if __name__ == '__main__':
     PATH = '../data'
     CONTENT_ONLY = False
+    REMOVE_TYPO = True
     df_index = ['ttr', 'root_ttr', 'log_ttr', 'maas_ttr', 'mattr', 'msttr', 'hdd', 'mtld', 'mtld_ma_bid',
                 'mtld_ma_wrap', 'token', 'type', 'raw', 'tokenized']
 
@@ -23,7 +25,7 @@ if __name__ == '__main__':
 
     # tokenize and calculate the LDs per text
     for index, txt in enumerate(txt_list):
-        pos_tuple_all, _, tokenized = tokenize(txt, content_only=CONTENT_ONLY, remove_typo=True)
+        pos_tuple_all, _, tokenized = tokenize(txt, content_only=CONTENT_ONLY, remove_typo=REMOVE_TYPO)
         # print to see output (TODO make it as argument)
         print(index)
         print(txt_id[index])
