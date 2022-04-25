890056c856e11969f4134683c04b8508e7e9d108
Sooyeon Cho <sy.cho2321@gmail.com>
2022-04-25 14:11:04 +0200

    modify korean_tokenizer.py

--- src/korean_tokenizer.py
+++ src/korean_tokenizer.py
@@ -1,96 +1,92 @@
-"""
-Do not change argument value content_only in func remove_stop_words.
-Do not change argument value remove_typo, content_only in func tokenize.
-"""
-import os
-import random
 from konlpy.tag import Okt, Komoran, Mecab, Kkma, Hannanum
-from vocabulary.vocab_clean import clean_vocab
 
-OKT_STOPWORDS = ['URL', 'Email', 'ScreenName', 'Hashtag', 'KoreanParticle', 'Punctuation', 'Foreign', 'Alpha', 'Number',
-                 'Unknown', 'PreEomi', 'Eomi']
+OKT_STOPWORDS = ["Punctuation", "Foreign", "Alpha", "Number", "Unknown", "KoreanParticle", "Hashtag", "ScreenName",
+                 "Email", "URL"]
+KOMORAN_STOPWORDS = ["SF", "SE", "SS", "SP", "SO", "SW", "SH", "SL", "SN", "NF", "NV", "NA"]
 
-KOMORAN_STOPWORDS = ['EP', 'EF', 'EC', 'ETN', 'ETM', 'SF', 'SE', 'SS', 'SP', 'SO', 'SW', 'SH', 'SL', 'SN', 'NF', 'NV',
-                     'NA']
-MECAB_STOPWORDS = ['EP', 'EF', 'EC', 'ETN', 'ETM', 'SF', 'SE', 'SSO', 'SSC', 'SC', 'SY', 'SH', 'SL', 'SN']
-KKMA_STOPWORDS = ['EPH', 'EPT', 'EPP', 'EFN', 'EFQ', 'EFO', 'EFA', 'EFI', 'EFR', 'ECE', 'ECS', 'ECD', 'ETN', 'ETD',
-                  'SF', 'SE', 'SS', 'SP', 'SO', 'SW', 'OH', 'OL', 'ON', 'UN']
+MECAB_STOPWORDS = ["SF", "SE", "SSO", "SSC", "SC", "SY", "SH", "SL", "SN"]
 
-HANNANUM_STOPWORDS = ['E','S','F']
+KKMA_STOPWORDS = ["SF", "SE", "SS", "SP", "SO", "SW", "OH", "OL", "ON", "UN"]
 
-CONTENT_POS = ['Noun', 'Verb', 'Adjective', 'Adverb']
+HANNANUM_STOPWORDS = ['S', 'F']
 
-# CLEAN_VOCAB = clean_vocab
+OKT_FUNCTIONWORDS = ["Josa", "PreEomi", "Eomi", "Suffix"]
 
-def remove_stop_words(token_pos_tuple, tokenizer, content_only=False):
+KOMORAN_FUNCTIONWORDS = ["JKS", "JKC", "JKG", "JKO", "JKB", "JKV", "JKQ", "JC", "JX", "EP", "EF", "EC", "ETN", "ETM",
+                         "XPN", "XSN", "XSV", "XSA"]
+
+MECAB_FUNCTIONWORDS = ["JKS", "JKC", "JKG", "JKO", "JKB", "JKV", "JKQ", "JC", "JX", "EP", "EF", "EC", "ETN", "ETM",
+                       "XPN", "XSN", "XSV", "XSA"]
+
+KKMA_FUNCTIONWORDS = ["JKS", "JKC", "JKG", "JKO", "JKM", "JKI", "JKQ", "JC", "JX", "EPH", "EPT", "EPP", "EFN", "EFQ",
+                      "EFO", "EFA", "EFI", "EFR", "ECE", "ECS", "ECD", "ETN", "ETD", "XPN", "XPV", "XSN", "XSV", "XSA"]
+
+HANNANUM_FUNCTIONWORDS = ["J", "E", "X"]
+
+
+def remove_pos(token_pos_tuple, pos_list):
     """
-    :param tokenizer: str, possible options: (okt, komoran, mecab, kkma, hannanum)
-    :param content_only: boolean, if True, only content words (verb, noun, adjective, adverb) are included in the output.
+    Remove given POSs in the tokenized tuple
     :param token_pos_tuple: list of tuple consisting of token and POS [('열심히', 'Adverb'), ('코딩', 'Noun')...]
-    :return: list of tokens without stopwords
+    :param pos_list: list of POS to be removed
+    :return: cleaned tuple of ('token', 'Part-Of-Speech') as list
     """
     pos_tuple_cleaned = []
 
-    if tokenizer == 'okt':
-        stopwords = OKT_STOPWORDS
-    elif tokenizer == 'komoran':
-        stopwords = KOMORAN_STOPWORDS
-    elif tokenizer == 'mecab':
-        stopwords = MECAB_STOPWORDS
-    elif tokenizer == 'kkma':
-        stopwords = KKMA_STOPWORDS
-    elif tokenizer == 'hannanum':
-        stopwords = HANNANUM_STOPWORDS
-    else:
-        raise ValueError("tokenizer must be one of these options: (okt, komoran, mecab, kkma, hannanum)")
-
-    if content_only:  # include only content words:
-        for index, pair in enumerate(token_pos_tuple):
-            if pair[1] in CONTENT_POS:  # tuple[1] = POS
-                pos_tuple_cleaned.append(pair)
-
-    else:  # include all words (function words + content words)
-        for index, pair in enumerate(token_pos_tuple):
-            if pair[1] not in stopwords:
-                pos_tuple_cleaned.append(pair)
+    for index, pair in enumerate(token_pos_tuple):
+        if pair[1] not in pos_list:
+            pos_tuple_cleaned.append(pair)
 
     return pos_tuple_cleaned
 
 
-def tokenize(tokenizer, data, content_only=False, remove_typo=False):
+def tokenize(tokenizer, text, include_function_words=True):
     """
     tokenize sequences using konlpy tokenizer.
     :param tokenizer: str, possible options: (okt, komoran, mecab, kkma, hannanum)
-    :param remove_typo: boolean if True, remove typo tokens using dictionary
-    :param data: str, raw text
-    :param content_only: boolean, if True, only content words (verb, noun, adjective, adverb) are included in the output.
-    :return:
+    :param text: str, raw text
+    :param include_function_words: bool
+                                    if set True: tokenize content + function words
+                                    if set False: tokenize only content words
+    :return: tuple (pos_tuple_all, pos_tuple_cleaned, tokens_cleaned)
+            where pos_tuple_all consists of tuple ('token', 'Part-Of-Speech') of all raw tokens (including stopwords like punctuation, numbers, URL ...)
+                  pos_tuple_cleaned consists of tuple ('token', 'Part-Of-Speech') of contents words (+ function words if the param include_function_words=True)
+                  tokens_cleaned is a list of stopword removed tokens (if the param include_function_words=True, function words are also included)
     """
     if tokenizer == 'okt':
         tagger = Okt()
+        stopwords = OKT_STOPWORDS
+        functionwords = OKT_FUNCTIONWORDS
     elif tokenizer == 'komoran':
         tagger = Komoran()
+        stopwords = KOMORAN_STOPWORDS
+        functionwords = KOMORAN_FUNCTIONWORDS
     elif tokenizer == 'mecab':
-        tagger = Mecab()  # todo Mecab not available in Windows. Do error handling.
+        tagger = Mecab()
+        stopwords = MECAB_STOPWORDS
+        functionwords = MECAB_FUNCTIONWORDS
     elif tokenizer == 'kkma':
         tagger = Kkma()
+        stopwords = KKMA_STOPWORDS
+        functionwords = KKMA_FUNCTIONWORDS
     elif tokenizer == 'hannanum':
         tagger = Hannanum()
+        stopwords = HANNANUM_STOPWORDS
+        functionwords = HANNANUM_FUNCTIONWORDS
     else:
         raise ValueError("tokenizer must be one of these options: (okt, komoran, mecab, kkma, hannanum)")
 
     # tokenize
-    if tokenizer == 'okt':
-        pos_tuple_all = tagger.pos(data, stem=True)
+    if tokenizer == 'okt':  # 'okt' tokenize has an argument 'stem'
+        pos_tuple_all = tagger.pos(text, stem=True)
     else:
-        pos_tuple_all = tagger.pos(data)
+        pos_tuple_all = tagger.pos(text)
 
     # remove stopwords
-    pos_tuple_cleaned = remove_stop_words(pos_tuple_all, tokenizer=tokenizer, content_only=False)
+    pos_tuple_cleaned = remove_pos(pos_tuple_all, pos_list=stopwords)
 
-    # remove typos
-    # if remove_typo:
-    #     pos_tuple_cleaned = [item for item in pos_tuple_cleaned if item in CLEAN_VOCAB]
+    if not include_function_words:  # remove function words
+        pos_tuple_cleaned = remove_pos(pos_tuple_cleaned, pos_list=functionwords)
 
     # separate lists for tokens
     tokens_cleaned = [item[0] for item in pos_tuple_cleaned]
@@ -98,34 +94,9 @@ def tokenize(tokenizer, data, content_only=False, remove_typo=False):
     return pos_tuple_all, pos_tuple_cleaned, tokens_cleaned
 
 
-# def write_tokenized_output(number_of_files=5):
-#     """
-#     TODO move this function to separate script
-#     :param number_of_files:
-#     :return:
-#     """
-#     random.seed(1)
-#
-#     while number_of_files > 0:
-#         number_of_files -= 1
-#         file = random.choice(os.listdir("../data/selected"))
-#         file_dir = "../data/selected/" + file
-#         with open(file_dir, encoding='utf-8') as f:
-#             lines = f.read()
-#             pos_all, _, tokenized = tokenize(lines)
-#             output_dir = "../data/output/" + file[:-4] + "_Output.txt"
-#             with open(output_dir, "w", encoding='utf-8') as output_file:
-#                 output_file.write(lines)
-#                 output_file.write('\n\n')
-#                 output_file.write(str(pos_all))
-#                 output_file.write('\n\n')
-#                 output_file.write(str(tokenized))
-
-
 if __name__ == '__main__':
-    txt = "안녕 이건 텍스트야! 예시 텍스트인데 나 어떄? 예뻐? 3000만큼 사랑해 ㅎㅎ"
-    pos_tuple_all, pos_tuple_cleaned, tokens_cleaned = tokenize(tokenizer='okt', data=txt, remove_typo=True,
-                                                                content_only=False)
+    txt = "먹다 먹어요 먹었어요 안녕 이건 텍스트야! 예시 텍스트인데 나 어떄? 예뻐? 3000만큼 사랑해"
+    pos_tuple_all, pos_tuple_cleaned, tokens_cleaned = tokenize(tokenizer='komoran', text=txt, include_function_words=True)
     print(pos_tuple_all)
     print(pos_tuple_cleaned)
     print(tokens_cleaned)
