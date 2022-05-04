484baba57e8132ca3d78f8aa7bba0a0b92649ec5
Sooyeon Cho <sy.cho2321@gmail.com>
2022-05-04 16:02:56 +0200

    change time representation in current_time_as_str()

--- src/util.py
+++ src/util.py
@@ -6,7 +6,12 @@ def current_time_as_str():
     read current time and process it to string to be used in a file name.
     :return: str, current time value
     """
-    return str(datetime.now()).replace(" ", '-').replace(":", '-')[:-7]
+    dateNtime = str(datetime.now()).replace(" ", '')
+    date = dateNtime[2:10].replace("-", "")
+    time = dateNtime[10:-10].replace(":", "")
+
+    return date + "_" + time
+
 
 def flatten_list(txts):
     """
